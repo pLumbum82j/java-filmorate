66685aa5acecf136e6bbe9d89d9a9cb4ead98d71
pLumbum82 <jobbsy@yandex.ru>
2023-01-19 11:36:31 +0500

    done Sprint #9 v.1.0

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -7,15 +7,16 @@ import ru.yandex.practicum.filmorate.validator.MovieBirthday;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 @Data
 @Builder
 public class Film {
     private Long id;
-    @NotBlank
+    @NotBlank(message = "Название фильма не может быть пустым")
     private String name;
-    @Max(value = 200, message = "Максимальная длина описания более 200 символов")
+    @Size(max = 200, message = "Максимальная длина описания более 200 символов")
     private String description;
     @MovieBirthday(message = "Дата релиза раньше 28 декабря 1895 года")
     private LocalDate releaseDate;
